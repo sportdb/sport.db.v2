8a47792d24978ab0560da4a77b99d2baadaef3b1
Gerald Bauer <gerald.bauer@gmail.com>
2026-03-15 14:54:05 +0100

    up teletype

--- parser/lib/sportdb/parser/lexer.rb
+++ parser/lib/sportdb/parser/lexer.rb
@@ -80,114 +80,38 @@ LINE_CONTINUATION_RE = %r{
                         }x
 
 
-########
-##   experimental teletype mode 
-##    only space, A-Z and 0-9 allowed
-IS_TTY_LINE_RE = %r{  \A  
-                       ## note - use NEGATIVE lookahead to exclude blank lines
-                         (?! [ ]*\z)
 
-                          [A-Z0-9 ]+
-                      \z
-                  }x
+###
+##  check for magic comments
+##     e.g  # teletype: true    or TELETYPE: TRUE 
+##             tty/teletype
 
-
-TTY_SPACES_RE = %r{ (?<spaces> [ ]{2,}) |
-                    (?<space>  [ ])
-                  }x
-TTY_NUM_RE    = %r{   \b  (?<num> \d+ ) \b 
-                  }x
-
-##
-##  note - TEXT for now allows    A, 1A, A1, A1A, A1 B1 C1, 
-##                                A1AA1 2B22 3C33
-##    - single space only for concat
-##       text segments MUST NOT be all numbers e.g. 1, 11, etc.                  
-TTY_TEXT_RE   = %r{   \b (?<text>                         
-                           (?:
-                              [A-Z]  ## MUST start with letter  
-                                |
-                               [0-9]+[A-Z]   ## or numbers followed by letter 
-                             )
-                             [0-9A-Z]*
-                             (?:
-                                 ### allow move segements separated
-                                 ##     by single space
-                                  [ ]
-                                 (?: 
-                                     [A-Z]  ## MUST start with letter  
-                                      |
-                                     [0-9]+[A-Z]   ## or numbers followed by letter 
-                                  )
-                                 [0-9A-Z]*
-                             )*
-                          )
-                          \b   
-                  }x 
+MAGIC_COMMENT_RE = %r{  \A
+                         [ ]*    ## optional leading spaces
+                        \#+      ##  note - allow ##,###, etc. too 
+                         [ ]*    ## optional spaces
+                           (?<magic_comment_key> tty | teletype )
+                         [ ]*    ## optional spaces
+                            :       
+                         [ ]*    ## optional spaces
+                            (?<magic_comment_value> true | false )
+                         [ ]*    ## optional trailing spaces
+                        \z
+                      }ix
 
 
-TTY_RE = Regexp.union(
-                TTY_SPACES_RE,
-                TTY_TEXT_RE,
-                TTY_NUM_RE,
-                ##  fix add ANY_RE,  
-)
-
-
-def _tokenize_tty_line( line )
-   line = line.strip
-
-   tokens = []
-   
-   ## track last offsets - to report error on no match
-   ##   or no match in end of string
-   offsets = [0,0]
-   pos = 0
-   m = nil   
- 
-
-  while m = TTY_RE.match( line, pos )
-    offsets = [m.begin(0), m.end(0)]
-
-    if offsets[0] != pos
-      ## match NOT starting at start/begin position!!!
-      ##  report parse error!!!
-      msg =  "!! WARN - tokenize (tty) error - skipping >#{line[pos..(offsets[0]-1)]}< @#{offsets[0]},#{offsets[1]} in line >#{line}<"
-      puts msg
-      log( msg )
-    end
-
-    pos = offsets[1]
-
-    t =  if m[:spaces] || m[:space] 
-               nil    ## skip spaces
-          elsif m[:text]
-            [:TTY_TEXT, m[:text]]
-          elsif m[:num]
-            [:TTY_NUM, m[:num].to_i(10)] 
-          else
-              ## report error/raise expection
-              puts "!!! TTY TOKENIZE ERROR - no match found"
-              nil 
-          end
-     
-    tokens << t    if t
-  end
-
-  ## check if no match in end of string
-  if offsets[1] != line.size
-      msg =  "!! WARN - tokenize (tty) error - skipping >#{line[offsets[1]..-1]}< @#{offsets[1]},#{line.size} in line >#{line}<"
-      puts msg
-      log( msg )
-  end
-
-  tokens
-end
 
 
 
 
 def tokenize_with_errors
+
+####
+##   flags / modes
+    @teletype = false     # use magic comment - tty/teletype: true
+
+
+
     tokens_by_line = []   ## note: add tokens line-by-line (flatten later)
     errors         = []   ## keep a list of errors - why? why not?
   
@@ -284,6 +208,37 @@ def tokenize_with_errors
   
 
     txt.each_line do |line|
+        ## line = line.rstrip   ## note - MUST remove/strip trailing newline (spaces optional)!!!
+        line = line.strip   ## note - strip leading AND trailing whitespaces
+                            ## note - trailing whitespace may incl. \n or \r\n!!!
+
+
+        ##
+        ###
+        ##  check for magic comments
+        ##     e.g  # teletype: true    or TELETYPE: TRUE 
+        ##             tty/teletype
+
+        if line.start_with?('#')   ###  skip comments (& check magic comments!!)
+           
+           if (m = MAGIC_COMMENT_RE.match(line))
+              magic_comment_key   = m[:magic_comment_key].downcase
+              magic_comment_value = m[:magic_comment_value].downcase
+
+              ##   turn on teletype mode
+              ## e.g.  tty: true  or teletype: true
+              if ['tty', 'teletype'].include?( magic_comment_key ) &&
+                 ['true'].include?( magic_comment_value )
+                 puts " magic comment - turn on teletype (tty) mode"
+                 @teletype = true
+              end
+           end
+
+           next
+        end
+
+        line = line.sub( /#.*/, '' ).strip   ###  cut-off end-of line comments too
+
 
        ##
        ##  first check for tabs
@@ -307,13 +262,6 @@ def tokenize_with_errors
                    '-'   ## replace with ascii dash (-)
                   end
 
-        ## line = line.rstrip   ## note - MUST remove/strip trailing newline (spaces optional)!!!
-        line = line.strip   ## note - strip leading AND trailing whitespaces
-                            ## note - trailing whitespace may incl. \n or \r\n!!!
-
-        next if line.start_with?('#')   ###  skip comments
-       
-        line = line.sub( /#.*/, '' ).strip   ###  cut-off end-of line comments too
 
 
         puts "line: >#{line}<"    if debug?
@@ -364,10 +312,12 @@ def tokenize_with_errors
            ##      hruler always resets parser mode to std/top-level!!!
            @re = RE
            tokens_by_line << [[:HRULER, '<|HRULER|>']]
-        elsif @re == RE && IS_TTY_LINE_RE.match(line)
+        elsif @teletype && (@re == RE && IS_TTY_LINE_RE.match(line))
             ## try experimental TELETYPE (TTY) mode!!!
+            ##    note - turn on via magic comment e.g.  tty/teletype: true
+            ###
             ###    move inside _tokenize_line - why? why not?
-            ##
+             
 
             tokens_by_line << _tokenize_tty_line( line )
 
