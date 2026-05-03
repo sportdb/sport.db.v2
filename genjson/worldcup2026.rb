d2ddd59b6e506995649deb8b21b7b4a52466f9e1
Gerald Bauer <gerald.bauer@gmail.com>
2026-05-03 19:02:33 +0200

    up

---
+++ genjson/worldcup2026.rb
@@ -0,0 +1,27 @@
+##
+##  to run use:
+##   $ ruby worldcup2026.rb        (in /genjson)
+
+require_relative 'helper'
+
+
+
+
+config_2026 = [
+  ['2026/worldcup.json',                ['worldcup/2026--usa/cup.txt',   'worldcup/2026--usa/cup_finals.txt']],
+  ['2026/worldcup.quali_playoffs.json', ['worldcup/2026--usa/quali_playoffs.txt']],
+]
+
+
+indir = OPENFOOTBALL_PATH
+## outdir =   "#{OPENFOOTBALL_PATH}/worldcup.json"
+outdir = './tmp-worldcup'
+
+config = config_2026
+
+
+genjson( config, outdir: outdir,
+                 indir: indir)
+
+
+puts "bye"
